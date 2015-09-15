2a35fdcc53b9241f73ba5b1a4536ed206a652b0c
podonoghue <podonoghue@swin.edu.au>
2015-09-15 12:50:33 +1000

    V4.12.1.10 - Eclipse Plugin Branding added

---
+++ UpdateSite/SignIt.cmd
@@ -0,0 +1,21 @@
+@echo off
+rem if "%1"=="" goto usage
+
+set /p PASS=Enter new Password:
+
+set KEYTOOL="c:\Program Files\Java\jdk1.8.0_60\bin\keytool.exe"
+set SIGNTOOL="c:\Program Files\Java\jdk1.8.0_60\bin\jarsigner.exe"
+set STORE="C:\Apps\Certificates\cacerts"
+set ALIAS=mykey
+set TIMESTAMP_URL=http://www.startssl.com/timestamp
+
+for %%f in (plugins\*.jar features\*.jar) do %SIGNTOOL% %%f  "%ALIAS%" -tsa %TIMESTAMP_URL% -keystore %STORE% -storepass %PASS% -keypass %PASS%
+goto done
+
+:usage
+echo.
+echo "Usage SignIt <password>"
+echo.
+
+:done
+pause
