19adbb0f460d67257efe3918e9ca03118694f702
podonoghue <podonoghue@swin.edu.au>
2016-03-13 01:38:52 +1100

    V4.12.1.90 - Improved CDT indexing

--- net.sourceforge.usbdm.cdt.tools/src/net/sourceforge/usbdm/cdt/tools/scanners/UsbdmGCCSpecsRunSIProvider.java
+++ net.sourceforge.usbdm.cdt.tools/src/net/sourceforge/usbdm/cdt/tools/scanners/UsbdmGCCSpecsRunSIProvider.java
@@ -1,34 +1,21 @@
 package net.sourceforge.usbdm.cdt.tools.scanners;
 
+import org.eclipse.cdt.make.core.scannerconfig.IExternalScannerInfoProvider;
+import org.eclipse.cdt.make.internal.core.scannerconfig2.GCCSpecsRunSIProvider;
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+import org.eclipse.cdt.managedbuilder.core.IToolChain;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
+import org.eclipse.core.resources.IProject;
+
 import net.sourceforge.usbdm.cdt.tools.UsbdmConstants;
 import net.sourceforge.usbdm.constants.ToolInformationData;
 import net.sourceforge.usbdm.constants.UsbdmSharedSettings;
 
-import org.eclipse.cdt.make.core.scannerconfig.IExternalScannerInfoProvider;
-import org.eclipse.cdt.make.internal.core.scannerconfig2.GCCSpecsRunSIProvider;
-import org.eclipse.cdt.managedbuilder.core.IBuildObject;
-import org.eclipse.cdt.managedbuilder.core.IConfiguration;
-import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
-import org.eclipse.cdt.managedbuilder.core.IOption;
-import org.eclipse.cdt.managedbuilder.core.ITool;
-import org.eclipse.cdt.managedbuilder.core.IToolChain;
-import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
-import org.eclipse.cdt.managedbuilder.internal.core.ResourceConfiguration;
-import org.eclipse.core.resources.IProject;
-
 @SuppressWarnings("restriction")
 public class UsbdmGCCSpecsRunSIProvider extends GCCSpecsRunSIProvider implements
-      IExternalScannerInfoProvider {
-
-   protected IToolChain getToolChain(ITool tool) {
-      IBuildObject parent = tool.getParent();
-      if(parent instanceof ResourceConfiguration) {
-         return ((ResourceConfiguration)parent).getBaseToolChain();
-      }
-      else {
-         return (IToolChain)parent;
-      }      
-   }
+IExternalScannerInfoProvider {
 
    /**
     * @param project       The project to look in for options
@@ -40,11 +27,11 @@ public class UsbdmGCCSpecsRunSIProvider extends GCCSpecsRunSIProvider implements
          return "";
       }
       IConfiguration cfg = info.getDefaultConfiguration();
-//      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() Found IConfiguration = " + cfg.getName());
+      //      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() Found IConfiguration = " + cfg.getName());
 
       IToolChain toolChain = cfg.getToolChain();
-//      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() Found toolChain = " + toolChain.getName());
-      
+      //      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() Found toolChain = " + toolChain.getName());
+
       // Find selected build tool (either ARM or Coldfire)
       IOption buildToolOption = toolChain.getOptionBySuperClassId(UsbdmConstants.ARM_BUILDTOOLS_OPTIONS);
       if (buildToolOption == null) {
@@ -53,7 +40,7 @@ public class UsbdmGCCSpecsRunSIProvider extends GCCSpecsRunSIProvider implements
       if (buildToolOption == null) {
          return "";
       }
-      
+
       // Get build path variable
       ToolInformationData toolData = ToolInformationData.getToolInformationTable().get(buildToolOption.getValue().toString());
       if (toolData == null) {
@@ -72,7 +59,7 @@ public class UsbdmGCCSpecsRunSIProvider extends GCCSpecsRunSIProvider implements
          toolPrefix = "Tool Prefix not set";
          return "";
       }
-//      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() Found tool prefix = " + toolPrefix);
+      //      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() Found tool prefix = " + toolPrefix);
       return toolPrefix;
 
    }
@@ -82,18 +69,18 @@ public class UsbdmGCCSpecsRunSIProvider extends GCCSpecsRunSIProvider implements
     */
    @Override
    protected boolean initialize() {
+      // Add tool prefix to command
+      buildInfo.setProviderRunCommand("specsFile", getToolPrefix(resource.getProject())+"gcc");
+
       if (!super.initialize()) {
          return false;
       }
 
-      // Add tool prefix to command
-//      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() fCompileCommand(Before) = \'" + fCompileCommand.toPortableString() + "\'");
-      String command = fCompileCommand.lastSegment();
-      if (command != null) {
-         command = getToolPrefix(resource.getProject())+command;
-         fCompileCommand = fCompileCommand.removeLastSegments(1).append(command);
-      }
-//      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() fCompileCommand(After) = \'" + fCompileCommand.toPortableString() + "\'");
+//      System.err.println("providerId = " + providerId);
+//      System.err.println("getProviderRunCommand = " + buildInfo.getProviderRunCommand(providerId));
+
+//      fCompileCommand = new Path(buildInfo.getProviderRunCommand(providerId));
+      System.err.println("UsbdmGCCSpecsRunSIProvider.initialize() fCompileCommand(After) = \'" + fCompileCommand.toPortableString() + "\'");
       return true;
    }
 }
