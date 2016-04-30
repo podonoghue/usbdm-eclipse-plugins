bced613edc451ef7e3f6583b1b3a7fe7d1ac2358
podonoghue <podonoghue@swin.edu.au>
2016-05-01 02:09:34 +1000

    V4.12.1.90 - Work on device editor

--- net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/editor/ValueColumnLabelProvider.java
+++ net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/editor/ValueColumnLabelProvider.java
@@ -1,30 +1,39 @@
 package net.sourceforge.usbdm.deviceEditor.editor;
 
-import org.eclipse.jface.viewers.ColumnLabelProvider;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
 
 import net.sourceforge.usbdm.deviceEditor.Activator;
 import net.sourceforge.usbdm.deviceEditor.model.BaseModel;
 import net.sourceforge.usbdm.deviceEditor.model.BinaryVariableModel;
-import net.sourceforge.usbdm.deviceEditor.model.SelectionModel;
 
-public class ValueColumnLabelProvider extends ColumnLabelProvider{
+class ValueColumnLabelProvider extends BaseLabelProvider {
+   
    private  Image lockedImage    = null;
    private  Image checkedImage   = null;
    private  Image uncheckedImage = null;
-   final    Color disabledColour = Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_LIGHT_SHADOW);
 
-   ValueColumnLabelProvider() {
-      super();
+   ValueColumnLabelProvider(TreeEditor viewer) {
       if (Activator.getDefault() != null) {
          lockedImage = Activator.getDefault().getImageDescriptor(Activator.ID_LOCKED_NODE_IMAGE).createImage();
          checkedImage = Activator.getDefault().getImageDescriptor(Activator.ID_CHECKBOX_CHECKED_IMAGE).createImage();
          uncheckedImage = Activator.getDefault().getImageDescriptor(Activator.ID_CHECKBOX_UNCHECKED_IMAGE).createImage();
       }
+   }
+
+   @Override
+   public String getText(BaseModel baseModel) {
+      return baseModel.getValueAsString();
+   }
+
+   @Override
+   public Image getImage(BaseModel baseModel) {
+      if (baseModel.isLocked()) {
+         return lockedImage;
+      }
+      if (baseModel instanceof BinaryVariableModel) {
+         return ((Boolean)((BinaryVariableModel)baseModel).getBooleanValue())?checkedImage:uncheckedImage;
+      }
+      return null;
    }
 
    @Override
@@ -40,49 +49,22 @@ public class ValueColumnLabelProvider extends ColumnLabelProvider{
       }
       return null;
    }
-
-   @Override
-   public String getText(Object element) {
-      return ((BaseModel) element).getValueAsString();
-   }
-
-   @Override
-   public Color getBackground(Object element) {
-      if (!((BaseModel) element).isEnabled()) {
-         return disabledColour;
-      }
-      return super.getBackground(element);
-   }
-
-   @Override
-   public Color getForeground(Object element) {
-      if (element instanceof SelectionModel) {
-         SelectionModel model = (SelectionModel) element;
-         if (model.isReset()) {
-            return Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_DARK_SHADOW);
-         }
-      }
-      else if (!((BaseModel) element).isEnabled()) {
-         return Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_DARK_SHADOW);
-      }
-      return super.getForeground(element);
-   }
-
-   @Override
-   public String getToolTipText(Object element) {
-      return ((BaseModel) element).getToolTip();
-   }
-
-   @Override
-   public Font getFont(Object element) {
-      return super.getFont(element);
-   }
-
+   
    @Override
    public void dispose() {
       super.dispose();
       if (lockedImage != null) {
          lockedImage.dispose();
+         lockedImage = null;
+      }
+      if (checkedImage != null) {
+         checkedImage.dispose();
+         checkedImage = null;
+      }
+      if (uncheckedImage != null) {
+         uncheckedImage.dispose();
+         uncheckedImage = null;
       }
    }
+
 }
