3a5dd4304c30e31710b76f12bb45a919307301e9
podonoghue <podonoghue@swin.edu.au>
2016-06-09 00:53:15 +1000

    V4.12.1.90 - Rebuild of plugins for release

--- net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/information/DeviceInfo.java
+++ net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/information/DeviceInfo.java
@@ -1,6 +1,5 @@
 package net.sourceforge.usbdm.deviceEditor.information;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- net.sourceforge.usbdm.jni/src/net/sourceforge/usbdm/jni/Activator.java
+++ net.sourceforge.usbdm.jni/src/net/sourceforge/usbdm/jni/Activator.java
@@ -1,39 +1,66 @@
 package net.sourceforge.usbdm.jni;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * @since 4.12
+ * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
 
-   private static BundleContext context;
+	// The plug-in ID
+	public static final String PLUGIN_ID = "net.sourceforge.usbdm.jni"; //$NON-NLS-1$
 
-   static BundleContext getContext() {
-      return context;
-   }
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
 
-   /*
-    * (non-Javadoc)
-    * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-    */
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
    @Override
-   public void start(BundleContext bundleContext) throws Exception {
-      super.start(bundleContext);
+   public void start(BundleContext context) throws Exception {
+      super.start(context);
       System.err.println(String.format("[%s, %s].start()", getBundle().getSymbolicName(), getBundle().getVersion()));
-      context = bundleContext;
+      plugin = this;
    }
 
-   /*
-    * (non-Javadoc)
-    * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-    */
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
    @Override
-   public void stop(BundleContext bundleContext) throws Exception {
-      context = null;
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
       Usbdm.exit();
       System.err.println(String.format("[%s, %s].stop()", getBundle().getSymbolicName(), getBundle().getVersion()));
-      super.stop(bundleContext);
+      super.stop(context);
    }
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
 }
