b0f483cbc74b273655efa3730de2d3eb0add73fe
podonoghue <podonoghue@swin.edu.au>
2016-07-11 12:32:03 +1000

    V4.12.1.120 - Work on SMC

--- net.sourceforge.usbdm.deviceEditor/src/Tests/TestDeviceEditor.java
+++ net.sourceforge.usbdm.deviceEditor/src/Tests/TestDeviceEditor.java
@@ -48,9 +48,9 @@ public class TestDeviceEditor {
 //         testEditor(Paths.get("projectFiles/Tests/FRDM_KL25Z.usbdmProject"));
 //         testEditor(Paths.get("projectFiles/Tests/FRDM_KL27Z.usbdmProject"));
 //         testEditor(Paths.get("projectFiles/Tests/FRDM_KL43Z.usbdmProject"));
-//         testEditor(Paths.get("projectFiles/Tests/FRDM_KL46Z.usbdmProject"));
+         testEditor(Paths.get("projectFiles/Tests/FRDM_KL46Z.usbdmProject"));
 //
-         testEditor(Paths.get("projectFiles/Tests/FRDM_K20D50M.usbdmProject"));
+//         testEditor(Paths.get("projectFiles/Tests/FRDM_K20D50M.usbdmProject"));
 //         testEditor(Paths.get("projectFiles/Tests/FRDM_K22F.usbdmProject"));
 //         testEditor(Paths.get("projectFiles/Tests/FRDM_K22F1M0.usbdmProject"));
 //         testEditor(Paths.get("projectFiles/Tests/FRDM_K64F.usbdmProject"));
