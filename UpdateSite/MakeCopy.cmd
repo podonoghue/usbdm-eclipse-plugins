1a52dcf4e81c8d1f68f6ab066be558584f0493a9
podonoghue <podonoghue@swin.edu.au>
2016-09-03 10:27:14 +1000

    V4.12.1.150 - Rebuild plugins

--- UpdateSite/MakeCopy.cmd
+++ UpdateSite/MakeCopy.cmd
@@ -2,6 +2,6 @@
 
 set HOME_DIR=%~dp0
 
-xcopy /e /i /y %HOME_DIR%\..\UpdateSite c:\Users\podonoghue\Documents\Development\VB_Shared\updateSite
+xcopy /e /i /y %HOME_DIR%\..\UpdateSite c:\Users\podonoghue\Documents\VB_Shared\updateSite
 
 pause
