c90456f1ee1c70c363dd055deb5ea7192aca752c
Peter O'Donoghue <podonoghue@swin.edu.au>
2017-07-18 13:51:49 +1000

    V4.12.1.190 - Corrected maximum count values

--- net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/validators/PitValidate.java
+++ net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/validators/PitValidate.java
@@ -58,7 +58,7 @@ public class PitValidate extends Validator {
                pit_ldval = 0;
             }
             else {
-               pit_ldval = Math.round(pit_period*busFrequency);
+               pit_ldval = Math.max(0, Math.round((pit_period*busFrequency)-1));
             }
          }
          else if (variable.equals(pit_frequencyVar)) {
@@ -69,19 +69,19 @@ public class PitValidate extends Validator {
                pit_ldval = 0;
             }
             else {
-               pit_ldval = Math.round(busFrequency/pit_frequency);
+               pit_ldval = Math.max(0, Math.round(busFrequency/pit_frequency-1));
             }
          }
       }
-      pit_periodVar.setMax(65535/busFrequency);
+      pit_periodVar.setMax((pit_ldvalVar.getMax()+1)/busFrequency);
       pit_ldvalVar.setValue(pit_ldval);
       if (pit_ldval == 0) {
          pit_periodVar.setValue(0);
          pit_frequencyVar.setValue(0);
       }
       else {
-         pit_periodVar.setValue(pit_ldval/busFrequency);
-         pit_frequencyVar.setValue(busFrequency/pit_ldval);
+         pit_periodVar.setValue((pit_ldval+1)/busFrequency);
+         pit_frequencyVar.setValue(busFrequency/(pit_ldval+1));
       }
    }
 }
