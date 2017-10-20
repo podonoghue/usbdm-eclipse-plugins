5b2a9abbcd8dd480c053856ab5a4cb6948875bbc
Peter O'Donoghue <podonoghue@swin.edu.au>
2017-10-21 07:56:34 +1100

    V4.12.1.210 - Added validation for buffered option

--- net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/validators/LpuartValidate.java
+++ net.sourceforge.usbdm.deviceEditor/src/net/sourceforge/usbdm/deviceEditor/validators/LpuartValidate.java
@@ -2,8 +2,10 @@ package net.sourceforge.usbdm.deviceEditor.validators;
 
 import java.util.ArrayList;
 
+import net.sourceforge.usbdm.deviceEditor.information.BooleanVariable;
+import net.sourceforge.usbdm.deviceEditor.information.IrqVariable;
+import net.sourceforge.usbdm.deviceEditor.information.LongVariable;
 import net.sourceforge.usbdm.deviceEditor.information.Variable;
-import net.sourceforge.usbdm.deviceEditor.model.Status;
 import net.sourceforge.usbdm.deviceEditor.peripherals.PeripheralWithState;
 
 /**
@@ -13,13 +15,16 @@ import net.sourceforge.usbdm.deviceEditor.peripherals.PeripheralWithState;
  *     lpuart
  */
 public class LpuartValidate extends PeripheralValidator {
-   
+
+   private final static String[] externalVariables = {
+   };
+
    public LpuartValidate(PeripheralWithState peripheral, ArrayList<Object> values) {
       super(peripheral);
    }
 
    /**
-    * Class to determine LPUART settings
+    * Class to validate LPUART settings
     * @throws Exception 
     */
    @Override
@@ -27,9 +32,29 @@ public class LpuartValidate extends PeripheralValidator {
       
       super.validate(variable);
 
-      Variable     clockSource = getVariable("clockSource");
-      clockSource.setStatus((Status)null);
-      clockSource.setOrigin("");
-      clockSource.enable(false);
+//      System.err.println("Uart.validate("+variable+")");
+      addToWatchedVariables(externalVariables);
+      
+      // Variables
+      //=================================
+      BooleanVariable   uartClassVar           =  getBooleanVariable("lpuartClass");
+      LongVariable      receiveBufferSizeVar   =  getLongVariable("receiveBufferSize");
+      LongVariable      transmitBufferSizeVar  =  getLongVariable("transmitBufferSize");
+
+      IrqVariable       txrxHandlerVar         =  getIrqVariable("irqHandlers");
+      
+      // Enable/disable parameters that depend on mode
+      boolean uartClass = uartClassVar.getValueAsBoolean();
+      
+      if (variable == uartClassVar) {
+//         System.err.println("uartClassVar = "+uartClassVar.getValueAsBoolean());
+         // Changed buffering
+         receiveBufferSizeVar.enable(uartClass);
+         transmitBufferSizeVar.enable(uartClass);
+         txrxHandlerVar.setLocked(uartClass);
+         if (uartClass) {
+            txrxHandlerVar.setValue(IrqVariable.CLASS_VALUE);
+         }
+      }
    }
 }
